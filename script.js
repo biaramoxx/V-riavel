d6a94d460712669d63aa8dc1c970774313ba75b1
Bianca Ramos <130471180+biaramoxx@users.noreply.github.com>
2023-07-05 19:56:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+alert("Aula de matemática");
+
+let x=5;
+var y=12;
+
+document.write('a soma de x + y é ', x + y,'<br>' )
+
+document.write('a soma de x + y é ', x - y, '<br>' )
+
+document.write('a soma de x + y é ', x * y, '<br>' )
+
+document.write('a soma de x + y é ', x / y, '<br>' )
+
+document.write('a soma de x + y é ', x == y, '<br>' )
+
+document.write('a soma de x + y é ', x < y, '<br>' )
+
+document.write('a soma de x + y é ', x > y, '<br>' )
+
+document.write('a soma de x + y é ', x != y, '<br>' )
+
